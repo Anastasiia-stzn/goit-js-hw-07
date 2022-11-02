075835418a4b383548391ddfca3cc6049c383bcd
Anastasiia-stzn <106701742+Anastasiia-stzn@users.noreply.github.com>
2022-11-02 15:13:46 +0200

    done

---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+    })
+    .join("");
+}
+
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
